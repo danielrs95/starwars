d2f675416ce5d5e653aa37664dae72400ba302ff
Daniel <danielrs9504@gmail.com>
2021-04-07 17:50:45 -0500

    1. Pobla los homeworld con el nombre del planeta 2. Comenta maps en HomeScreen temporalmente 3. Comenta product name y homeworld en el componente product temporalmente

--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -20,17 +20,38 @@ function getAllStarwarsPeople() {
       let promises = [];
       // start at 2 as you already queried the first page
       for (let i = 2; i <= numberOfPagesLeft; i++) {
-        promises.push(axios(`https://swapi.dev/api/people?page=${i}`));
+        promises.push(axios(`https://swapi.dev/api/people?page=${i}`)); // Crea las peticiones extra al api
       }
+      // Crea un array con las respuestas de las peticiones creadas anteriormente
       return Promise.all(promises);
     })
     .then((response) => {
+      // console.log(response);
       //get the rest records - pages 2 through n.
       people = response.reduce(
         (acc, data) => [...acc, ...data.data.results],
         people
       );
+      // console.log(people);
+      // console.log(people.length);
       return people;
+    })
+    .then((response) => {
+      // console.log(response);
+      // Hacemos el loop sobre las personas
+      for (let i = 0; i < response.length; i++) {
+        let url = response[i].homeworld; // Obtenemos el url al cual haremos la peticion para cada una de las personas
+
+        // Async para poder resolver las promesas de axios
+        const getHomeworld = async (url) => {
+          let homeworldName = await axios.get(url); // obtenemos el objeto data de la peticion axios
+          response[i].homeworld = homeworldName.data.name;
+        };
+
+        getHomeworld(url); //Llamamos la funcion para setear los valores
+      }
+
+      console.log(response);
     })
     .catch((error) => console.log("Properly handle your exception here"));
 }
@@ -40,7 +61,7 @@ export const listProducts = () => async (dispatch) => {
     dispatch({ type: PRODUCT_LIST_REQUEST });
 
     const data = await getAllStarwarsPeople();
-    console.log(data);
+    // console.log(data);
 
     dispatch({
       type: PRODUCT_LIST_SUCCESS,
