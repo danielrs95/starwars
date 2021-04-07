b8f91ed5e57174f47c9cee84143258b23c45c03e
Daniel <danielrs9504@gmail.com>
2021-04-06 21:30:21 -0500

    Agrega peticion axios para recolectar toda la data

--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -5,25 +5,53 @@ import {
   PRODUCT_LIST_SUCCESS,
 } from "../constants/productConstants";
 
+function getAllStarwarsPeople() {
+  let people = [];
+  // first page
+  return axios("https://swapi.dev/api/people/")
+    .then((response) => {
+      // collect people from first page
+      people = response.data.results;
+      return response.data.count;
+    })
+    .then((count) => {
+      // exclude the first request
+      const numberOfPagesLeft = Math.ceil((count - 1) / 10);
+      let promises = [];
+      // start at 2 as you already queried the first page
+      for (let i = 2; i <= numberOfPagesLeft; i++) {
+        promises.push(axios(`https://swapi.dev/api/people?page=${i}`));
+      }
+      return Promise.all(promises);
+    })
+    .then((response) => {
+      //get the rest records - pages 2 through n.
+      people = response.reduce(
+        (acc, data) => [...acc, ...data.data.results],
+        people
+      );
+      return people;
+    })
+    .catch((error) => console.log("Properly handle your exception here"));
+}
+
 export const listProducts = () => async (dispatch) => {
   try {
     dispatch({ type: PRODUCT_LIST_REQUEST });
 
-    const { data } = await axios.get(`https://swapi.dev/api/people/`);
+    const data = await getAllStarwarsPeople();
     console.log(data);
-    const { results } = data;
-    console.log(results);
 
     dispatch({
       type: PRODUCT_LIST_SUCCESS,
-      payload: results,
+      payload: data,
     });
   } catch (error) {
     dispatch({
       type: PRODUCT_LIST_FAIL,
       payload:
-        error.response && error.response.results.message
-          ? error.response.results.message
+        error.response && error.response.data.message
+          ? error.response.data.message
           : error.message,
     });
   }
