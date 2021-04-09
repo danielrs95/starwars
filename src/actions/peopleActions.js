1714c26a40b0142b794a969d92f62ec22d7e8d62
Daniel <danielrs9504@gmail.com>
2021-04-09 05:52:25 -0500

    Popula vehiculos, naves y peliculas del personaje

--- src/actions/peopleActions.js
+++ src/actions/peopleActions.js
@@ -57,17 +57,29 @@ export const getPeopleDetails = (id) => async (dispatch) => {
     dispatch({ type: PEOPLE_DETAILS_REQUEST });
 
     const { data } = await axios.get(`https://swapi.dev/api/people/${id}`);
+    // Obtenemos el nombre del planeta de la persona
+    let homeworld = await axios.get(data.homeworld);
+    data.homeworld = homeworld.data.name;
+    console.log(data);
 
-    // let people = request.data.results;
-    // // Populamos el nombre del planeta
-    // for (let i = 0; i < people.length; i++) {
-    //   let planets = "";
-    //   // Hacemos la petición para popular people.homeworld
-    //   planets = await axios(people[i].homeworld);
-    //   people[i].homeworld = planets.data.name;
-    //   // Agregamos el ID al array de people, con un regex obetenmos solo el digito
-    //   people.forEach((item) => (item.id = item.url.match(/\d/g).join("")));
-    // }
+    // Obtenemos las peliculas iterando sobre las url del array
+    let filmCount = data.films.length;
+    for (let i = 0; i < filmCount; i++) {
+      let film = await axios.get(data.films[i]);
+      data.films[i] = film.data.title;
+    }
+
+    // Obtenemos los vehiculos
+    for (let i = 0; i < data.vehicles.length; i++) {
+      let vehicle = await axios.get(data.vehicles[i]);
+      data.vehicles[i] = vehicle.data.name;
+    }
+
+    // Obtenemos las naves
+    for (let i = 0; i < data.starships.length; i++) {
+      let shatship = await axios.get(data.starships[i]);
+      data.starships[i] = shatship.data.name;
+    }
 
     dispatch({
       type: PEOPLE_DETAILS_SUCCESS,
